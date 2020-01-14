9eda59e0ca9ab3834234af740b57ada751cb35d8
faturita <rramele@gmail.com>
2020-01-14 18:23:25 -0300

    All current tests were passed

--- testall.cpp
+++ testall.cpp
@@ -14,16 +14,16 @@
 int main(int argc, char *argv[])
 {
     char cmd[256];
-    int tests[10];
+    int tests[20];
     memset(tests,-1,sizeof(int));
 
-    for(int i=0;i<10;i++)
+    for(int i=0;i<19;i++)
     {
-        sprintf(cmd, "./waku -test %d",i+1);
+        sprintf(cmd, "./test -test %d",i+1);
         tests[i] = system(cmd);
     }
 
-    for(int i=0;i<10;i++)
+    for(int i=0;i<19;i++)
     {
         printf("Test:%2d\tResult:%s\n", i, ((tests[i]!=-1) ?"Ok" : "ERROR"));
     }
