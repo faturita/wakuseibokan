4be6541cbc4903951a34fc554271f218f709797f
faturita <rramele@gmail.com>
2021-01-05 12:37:27 -0300

    Now the standard Manta has a new model.

---
+++ units/AdvancedManta.h
@@ -0,0 +1,23 @@
+#ifndef ADVANCEDMANTA_H
+#define ADVANCEDMANTA_H
+
+#include "SimplifiedDynamicManta.h"
+
+class AdvancedManta : public SimplifiedDynamicManta
+{
+
+public:
+    AdvancedManta(int newfaction);
+
+    void  init();
+
+    int virtual getSubType();
+
+    void drawModel(float yRot, float xRot, float x, float y, float z);
+
+    void embody(dWorldID world, dSpaceID space);
+    void embody(dBodyID myBodySelf);
+    void getViewPort(Vec3f &Up, Vec3f &position, Vec3f &forward);
+};
+
+#endif // ADVANCEDMANTA_H
