4fc3f0ec032d7c6f92d4826b4fd203f44ff9cae5
faturita <rramele@gmail.com>
2024-02-04 15:15:48 -0300

    Fixing path in script.

--- system/linux/install.sh
+++ system/linux/install.sh
@@ -2,8 +2,8 @@ sudo mkdir /usr/games/waku
 sudo mv waku.tgz /usr/games/waku
 cd /usr/games/waku
 sudo tar xvzf waku.tgz
-sudo cp dependencies/*.lib /usr/local/lib
-sudo cp waku.desktop /usr/share/applications/waku.desktop
+sudo cp dependencies/lib* /usr/local/lib
+sudo cp system/linux/waku.desktop /usr/share/applications/waku.desktop
 sudo cp images/waku.png /usr/games/waku/waku.png
 chmod +x waku
 
